9d35d7e49526235563bb86deabc441e7bd208a4b
SofiaRin <bujie02@sina.com>
2016-09-15 23:27:01 +0800

    no message

--- draw.ts
+++ draw.ts
@@ -2,23 +2,19 @@ import readline = require("readline")
 
 function draw(num:number) {
 //var i :number = num - 1
-var a :number = 1;
-var b :number = 1;
-var c :number = 1;
+var a,b,c:number
+for(a = 0 ; a < num; a++) { //0-层数num 共num次打印 结尾换行
+
+     for( b = 0; b < num - a - 1; b++) { // num - a - 1 第a层对应空格数
+         console.log(" ")
+     }
+     for( c = 0; c < 2*a+1; c++) {
+         console.log("*")
+         console.log("\n")
+     }
+  }
+}
 console.log("How many layers should be drawn?")
-
-for(a; a <= num; a++) {
-
-    for( b; b <= num - a; b++) {
-        console.log(" ")
-    }
-    for( c; c < 2*a-1; c++)
-        console.log("*")
-        console.log("\n")
-
-}
-
-}
 var x:number  =  
 
 draw(x)
