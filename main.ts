5e39e40f8a262f316f0108fe9005e219fc293688
SofiaRin <bujie02@sina.com>
2016-09-21 22:30:19 +0800

    重新命名变量，改回了最开始写入node.js API的方法

--- main.ts
+++ main.ts
@@ -1,19 +1,166 @@
-//由于创建项目时候没通过新建项目的方法，而是用右键，新建文件改后缀的方式，使得找不到process
-//我从另一个新建的文件中拷贝了头文件，经测试可以读入键盘输入的操作数
-function draw(num:number) {
 
-var a,b,c:number;
+interface NodeBuffer {
+    [index: number]: number;
+    write(string: string, offset?: number, length?: number, encoding?: string): number;
+    toString(encoding?: string, start?: number, end?: number): string;
+    length: number;
+    copy(targetBuffer: NodeBuffer, targetStart?: number, sourceStart?: number, sourceEnd?: number): void;
+    slice(start?: number, end?: number): NodeBuffer;
+    readUInt8(offset: number, noAsset?: boolean): number;
+    readUInt16LE(offset: number, noAssert?: boolean): number;
+    readUInt16BE(offset: number, noAssert?: boolean): number;
+    readUInt32LE(offset: number, noAssert?: boolean): number;
+    readUInt32BE(offset: number, noAssert?: boolean): number;
+    readInt8(offset: number, noAssert?: boolean): number;
+    readInt16LE(offset: number, noAssert?: boolean): number;
+    readInt16BE(offset: number, noAssert?: boolean): number;
+    readInt32LE(offset: number, noAssert?: boolean): number;
+    readInt32BE(offset: number, noAssert?: boolean): number;
+    readFloatLE(offset: number, noAssert?: boolean): number;
+    readFloatBE(offset: number, noAssert?: boolean): number;
+    readDoubleLE(offset: number, noAssert?: boolean): number;
+    readDoubleBE(offset: number, noAssert?: boolean): number;
+    writeUInt8(value: number, offset: number, noAssert?: boolean): void;
+    writeUInt16LE(value: number, offset: number, noAssert?: boolean): void;
+    writeUInt16BE(value: number, offset: number, noAssert?: boolean): void;
+    writeUInt32LE(value: number, offset: number, noAssert?: boolean): void;
+    writeUInt32BE(value: number, offset: number, noAssert?: boolean): void;
+    writeInt8(value: number, offset: number, noAssert?: boolean): void;
+    writeInt16LE(value: number, offset: number, noAssert?: boolean): void;
+    writeInt16BE(value: number, offset: number, noAssert?: boolean): void;
+    writeInt32LE(value: number, offset: number, noAssert?: boolean): void;
+    writeInt32BE(value: number, offset: number, noAssert?: boolean): void;
+    writeFloatLE(value: number, offset: number, noAssert?: boolean): void;
+    writeFloatBE(value: number, offset: number, noAssert?: boolean): void;
+    writeDoubleLE(value: number, offset: number, noAssert?: boolean): void;
+    writeDoubleBE(value: number, offset: number, noAssert?: boolean): void;
+    fill(value: any, offset?: number, end?: number): void;
+    INSPECT_MAX_BYTES: number;
+}
+
+declare class EventEmitter {
+    addListener(event: string, listener: Function);
+    on(event: string, listener: Function);
+    once(event: string, listener: Function): void;
+    removeListener(event: string, listener: Function): void;
+    removeAllListeners(event: string): void;
+    setMaxListeners(n: number): void;
+    listeners(event: string): { Function; }[];
+    emit(event: string, ...args: any[]): void;
+}
+
+declare var SlowBuffer: {
+    new (str: string, encoding?: string): NodeBuffer;
+    new (size: number): NodeBuffer;
+    new (array: any[]): NodeBuffer;
+    prototype: NodeBuffer;
+    isBuffer(obj: any): boolean;
+    byteLength(string: string, encoding?: string): number;
+    concat(list: NodeBuffer[], totalLength?: number): NodeBuffer;
+};
+
+declare var Buffer: {
+    new (str: string, encoding?: string): NodeBuffer;
+    new (size: number): NodeBuffer;
+    new (array: any[]): NodeBuffer;
+    prototype: NodeBuffer;
+    isBuffer(obj: any): boolean;
+    byteLength(string: string, encoding?: string): number;
+    concat(list: NodeBuffer[], totalLength?: number): NodeBuffer;
+}
+
+declare class WritableStream extends EventEmitter {
+    writable: boolean;
+    write(str: string, encoding?: string, fd?: string): boolean;
+    write(buffer: NodeBuffer): boolean;
+    end(): void;
+    end(str: string, enconding: string): void;
+    end(buffer: NodeBuffer): void;
+    destroy(): void;
+    destroySoon(): void;
+}
+
+declare class ReadableStream extends EventEmitter {
+    readable: boolean;
+    setEncoding(encoding: string): void;
+    pause(): void;
+    resume(): void;
+    destroy(): void;
+    pipe(destination: WritableStream, options?: { end?: boolean; }): void;
+}
+
+declare class NodeProcess extends EventEmitter {
+    stdout: WritableStream;
+    stderr: WritableStream;
+    stdin: ReadableStream;
+    argv: string[];
+    execPath: string;
+    abort(): void;
+    chdir(directory: string): void;
+    cwd(): string;
+    env: any;
+    exit(code?: number): void;
+    getgid(): number;
+    setgid(id: number): void;
+    getuid(): number;
+    setuid(id: number): void;
+    version: string;
+    versions: { http_parser: string; node: string; v8: string; ares: string; uv: string; zlib: string; openssl: string; };
+    config: {
+        target_defaults: {
+            cflags: any[];
+            default_configuration: string;
+            defines: string[];
+            include_dirs: string[];
+            libraries: string[];
+        };
+        variables: {
+        clang: number;
+        host_arch: string;
+        node_install_npm: boolean;
+        node_install_waf: boolean;
+        node_prefix: string;
+        node_shared_openssl: boolean;
+        node_shared_v8: boolean;
+        node_shared_zlib: boolean;
+        node_use_dtrace: boolean;
+        node_use_etw: boolean;
+        node_use_openssl: boolean;
+        target_arch: string;
+        v8_no_strict_aliasing: number;
+        v8_use_snapshot: boolean;
+        visibility: string;
+    };
+    };
+    kill(pid: number, signal?: string): void;
+    pid: number;
+    title: string;
+    arch: string;
+    platform: string;
+    memoryUsage(): { rss: number; heapTotal; number; heapUsed: number; };
+    nextTick(callback: Function): void;
+    umask(mask?: number): number;
+    uptime(): number;
+    hrtime(): number[];
+    hrtime(start: number[]): number[];
+}
+
+declare var process: NodeProcess;
+
+function DrawPyramid(totalrows:number) {
+
+var currentRow,spaceCount,starCount:number;
 var spacestr, starstr, finalstr:string;
 
-for(a = 0 ; a < num; a++) { //0-层数num 共num次打印 结尾换行
+for( currentRow = 0 ; currentRow < totalrows; currentRow++) { 
      spacestr = "";
      starstr = "";
      finalstr = "";//若不在循环内定义，每一行的串前会显示undefine
 
-     for( b = 0; b < (num - a - 1); b++) { // num - a - 1 第a层对应空格数
+     for( spaceCount = 0; spaceCount < (totalrows - currentRow - 1); spaceCount++) { 
          spacestr  = spacestr + " ";  //组合空格串
      }
-     for( c = 0; c < (2*a+1); c++) {
+     for( starCount = 0; starCount < (2*currentRow+1); starCount++) {
          starstr = starstr + "*";  //组合星号串
      }
      
@@ -24,6 +171,6 @@ for(a = 0 ; a < num; a++) { //0-层数num 共num次打印 结尾换行
 }
 
 
-var x:string  = process.argv[2];  
+var rows:string  = process.argv[2];  
 
-draw(parseInt(x));
+DrawPyramid(parseInt(rows));
